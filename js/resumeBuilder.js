55503ebd9c9179abebae0d6b8cdca8dcf18396e7
Heather Nuffer <zzzuzz@gmail.com>
2016-06-30 21:44:17 -0600

    Add awesome message

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,3 +3,6 @@ This is empty on purpose! Your code to build the resume will go here.
  */
 
 $("#main").append("Heather Nuffer");
+var awesomeThoughts;
+awesomeThoughts = "I am Heather Nuffer, and I am AWESOME!";
+console.log(awesomeThoughts);
