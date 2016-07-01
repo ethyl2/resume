1ed77bc402afdd299510459158d1855bf7f89810
Heather Nuffer <zzzuzz@gmail.com>
2016-06-30 22:13:37 -0600

    Add formatted name, role to header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,8 +1,14 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var formattedName = HTMLheaderName.replace("%data%", "Heather Nuffer");
+var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
 
+$("#header").prepend(formattedName);
+$("#header").append(formattedRole);
+
+$("#main").append("<br>");
 $("#main").append("Heather Nuffer");
 var awesomeThoughts;
 awesomeThoughts = "I am Heather Nuffer, and I am AWESOME!";
 console.log(awesomeThoughts);
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+$("#main").append("<br>");
+$("#main").append(funThoughts);
