924acf3da833ec1919a55b07163f4c9d629a09b5
Heather Nuffer <zzzuzz@gmail.com>
2016-07-17 21:09:22 -0600

    Reformat contact info as object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -10,8 +10,13 @@ $("#header").prepend(formattedName);
 var bio = {
   "name": "Heather Nuffer",
   "role": "Web Developer",
-  "contactInfo": "zzzuzz@gmail.com/801-919-6515",
-  "pictureURL": "images/Heather.jpg",
+  "contactInfo": {
+    "email": "zzzuzz@gmail.com",
+    "cell": "801-919-6515",
+    "twitter": "@ethyl2",
+    "gitHub": "ethyl2"
+  },
+  "pictureURL": 'images/Heather.jpg',
   "welcomeMessage": "Welcome to my site!",
   "skills": ["HTML", "CSS", "JavaScript", "Python", "R", "technical writing",
     "art", "sewing", "music", "teaching"],
@@ -21,10 +26,11 @@ $("#main").append("Hello! I am " + bio.name + ", " + bio.role + ".");
 $("#main").append(" " + bio.welcomeMessage + "<br>");
 
 $("#main").append("<img src='images/Heather.jpg' alt='Heather Nuffer picture' style='float: right' width=200>");
+
 $("#main").append("My skills include: <br>");
 
 for (var i=0; i<bio.skills.length; i++) {
   $("#main").append(bio.skills[i] + "<br>");
 }
 
-$("#main").append(bio.contactInfo);
+$("#main").append(bio.contactInfo.email + " " + bio.contactInfo.cell);
