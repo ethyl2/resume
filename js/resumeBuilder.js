ad93360a354d6b93253e85aa376bad3bb0dbe2b6
Heather Nuffer <zzzuzz@gmail.com>
2016-07-18 21:34:24 -0600

    Add JSON education object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -28,15 +28,62 @@ work.employer = "Foothill Elementary PTA";
 work.yearsWorked = "2";
 work.cityOfBusiness = "Orem, UT";
 
-var education = {};
-education.name = "Brigham Young University";
-education.yearsAttended = "1994-1999";
-education.schoolsCity = "Provo, UT";
+var education1 = {};
+education1.name = "Brigham Young University";
+education1.yearsAttended = "1994-1999";
+education1.schoolsCity = "Provo, UT";
+
+var education = {
+  "schools" : [
+    {"name": "Brigham Young University",
+      "city": "Provo, UT",
+      "major": "Microbiology",
+      "minor": "Chemistry",
+      "graduationYear": "1999"
+    },
+    {"name": "Stanford University",
+      "city": "Stanford, CA",
+      "courseraCertificate": "Child Nutrition and Cooking 2.0",
+      "year": "2013"
+    },
+    {"name": "The Pennsylvania State University",
+      "city": "State College, PA",
+      "courseraCertificate": "Introduction to Art: Concepts & Techniques",
+      "year": "2013"
+    },
+    {"name": "The Hong Kong University of Science and Technology",
+      "city": "Clear Water Bay, Hong Kong",
+      "courseraCertificate" : "The Science of Gastronomy",
+      "year": "2013"
+    },
+    {"name": "University of Toronto",
+      "city": "Toronto, Ontario",
+      "courseraCertificate": "Learn to Program: The Fundamentals",
+      "year": "2014"
+    },
+    {"name": "Rice University",
+      "city": "Houston, TX",
+      "courseraCertificate" :["An Introduction to Interactive Programming in Python",
+        "Principles of Computing"],
+      "year": "2014"
+    },
+    {"name": "John Hopkins University",
+      "city": "Baltimore, MD",
+      "courseraCertificate" : ["The Data Scientist's Toolbox", "R Programming",
+        "Getting and Cleaning Data", "Exploratory Data Analysis", "Reproducible Research"],
+      "year": "2015"
+    },
+    {"name": "Udacity",
+      "nanodegree": "Front-End Web Development",
+      "year": "2016"
+    }
+  ]
+}
 
 $("#main").append("Hello! I am " + bio.name + ", " + bio.role + ".");
 $("#main").append(" " + bio.welcomeMessage + "<br>");
 
-$("#main").append("<img src='images/Heather.jpg' alt='Heather Nuffer picture' style='float: right' width=200>");
+$("#main").append("<img src=images/Heather.jpg alt='Heather Nuffer picture' style='float: right' width=200>");
 
 $("#main").append("My skills include: <br>");
 
@@ -44,5 +91,5 @@ for (var i=0; i<bio.skills.length; i++) {
   $("#main").append(bio.skills[i] + "<br>");
 }
 
-$("#main").append(work["position"] + " " + education.name + "<br>");
+$("#main").append(work["position"] + " " + education1.name + "<br>");
 $("#main").append(bio.contactInfo.email + " " + bio.contactInfo.cell);
